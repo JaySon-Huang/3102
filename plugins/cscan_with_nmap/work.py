63d345e526aab7228d43440777b35b7d46505b4c
JaySon-Huang <jayson.hjs@gmail.com>
2015-01-18 01:57:21 +0800

    minor change

--- plugins/cscan_with_nmap/work.py
+++ plugins/cscan_with_nmap/work.py
@@ -12,15 +12,17 @@ class cscan_with_nmap(Plugin):
     def start(self, domain, domain_type, level):
         super(cscan_with_nmap, self).start(domain, domain_type, level)
         try:
+            # import pdb;pdb.set_trace()
             nm = PortScanner()
-        except PortScannerError, e:
+        except nmap.PortScannerError:
             # 没有检测到namp
             print "Please Install nmap first."
-            raise e
-        except:
             raise
-        print 'running cscan'
+        except:
+            print "Unknow exception"
+            raise
         nm.scan(hosts=domain+'/24', arguments='-sP -sV')
+
         self.result = {
             'root_domain': [],
             'ip': nm.all_hosts(),
